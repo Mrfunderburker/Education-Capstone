857d34cbabdf5e5a01ea7b696165358cc32ac0a5
Khadazhaia Grayson <khadazhaia.grayson@outlook.com>
2025-07-02 21:57:11 -0400

    adding transformations

--- code/etl.py
+++ code/etl.py
@@ -13,7 +13,7 @@ def extract_csv():
 
 def transform_attendance(attendance):
     """
-    - Remove suppressed data ("s" values)
+    - Remove suppressed data ("s" values) and "All Students" data
     - Convert object types to integers or floats
     """
 
@@ -21,18 +21,22 @@ def transform_attendance(attendance):
     suppressed_rows = attendance.isin(["s"]).any(axis=1)
     attendance = attendance[~suppressed_rows]
     
+    # Drop all students rows 
+    attendance = attendance.drop(attendance[attendance["Category"] == "All Students"].index)
+
     # Convert objects to numeric columns
     numeric_columns = [col for col in attendance.columns if "#" in col or "%" in col or "Grade" in col]
     for col in numeric_columns:
-        attendance[col] = pd.to_numeric(attendance[col], errors="coerce")
-        if "#" in col or "Grade" in col:
-            attendance[col] = attendance[col].astype("Int64")
+         attendance[col] = pd.to_numeric(attendance[col], errors="coerce")
+         if "#" in col or "Grade" in col:
+             attendance[col] = attendance[col].astype("Int64")
+
 
     return attendance
 
 def transform_graduation(graduation):
     """
-    - Remove suppressed and "Category" data
+    - Remove suppressed, "Category" and "All Students" data 
     - Convert object types to integers or floats
     - Calculate cohort duration and graduation year
     """
@@ -41,16 +45,19 @@ def transform_graduation(graduation):
     suppressed_rows = graduation.isin(["s"]).any(axis=1)
     graduation = graduation[~suppressed_rows]
 
-    # Dropped "Category" rows
+    # Drop "Category" rows
     rows_to_drop = graduation.isin(["Category"]).any(axis=1)
     graduation  = graduation[~rows_to_drop]
+
+    # Drop all students rows 
+    graduation = graduation.drop(graduation[graduation["Category"] == "All Students"].index)
 
     # Convert objects to numeric columns
     numeric_columns = [col for col in graduation.columns if "#" in col or "%" in col or "Cohort Year" in col]
     for col in numeric_columns:
-        graduation[col] = pd.to_numeric(graduation[col], errors="coerce")
-        if "#" in col  or "Cohort Year" in col :
-            graduation[col] = graduation[col].astype("Int64")
+         graduation[col] = pd.to_numeric(graduation[col], errors="coerce")
+         if "#" in col  or "Cohort Year" in col :
+             graduation[col] = graduation[col].astype("Int64")
     
     # Calculate cohort duration and graduation year
     graduation["Cohort Duration"] = graduation["Cohort"].str.extract(r"(\d+)").astype(int)
@@ -59,6 +66,43 @@ def transform_graduation(graduation):
     return graduation
 
 def transform_regents(regents):
+    """
+    - Remove suppressed, "Category" and "All Students" data 
+    - Drop unnecessary columns 
+    - Convert object types to integers or floats
+    """
+
+    # Remove suppressed rows
+    suppressed_rows = regents.isin(['s']).any(axis=1)
+    regents = regents[~suppressed_rows]
+
+    # Drop "Category" rows
+    rows_to_drop = regents.isin(["Category"]).any(axis=1)
+    regents = regents[~rows_to_drop]
+
+    # Drop all students rows 
+    regents = regents.drop(regents[regents["Category"] == "All Students"].index)
+  
+    # Drop unnecesary columns
+    regents = regents.drop(["School DBN", "School Name", "School Type", "School Level", "Number meeting CUNY proficiency requirmenets", "Percent meeting CUNY proficiency requirmenets"], axis=1)
+    
+    # Change objects to numeric columns
+    numeric_columns = [
+    "Year",
+    "Total Tested", 
+    "Mean Score",
+    "Number Scoring Below 65", 
+    "Percent Scoring Below 65",
+    "Number Scoring 65 or Above",
+    "Percent Scoring 65 or Above",
+    "Number Scoring 80 or Above", 
+    "Percent Scoring 80 or Above",
+]
+
+    for col in numeric_columns:
+         regents[col] = pd.to_numeric(regents[col], errors="coerce")
+         if regents[col].dropna().apply(lambda x: float(x).is_integer()).all():
+             regents[col] = regents[col].astype("Int64")
 
     return regents
 
